c1dedf206f1ebd4d19d8d7dd22ed7744e1210c89
Vivixing <echeverri.vivian1120@gmail.com>
2025-01-30 18:46:04 -0500

    Creado repository de Usuario

---
+++ repository/UsuarioRepository.py
@@ -0,0 +1,17 @@
+from models.UsuarioModel import Usuario
+
+
+class UsuarioRepository:
+
+    @staticmethod
+    async def create_user(new_user: Usuario) -> Usuario:
+        return await new_user.insert()
+    
+    @staticmethod
+    async def find_user_by_email(email: str) -> Usuario:
+        return await Usuario.find_one(Usuario.email == email)
+    
+    @staticmethod
+    async def find_user_by_id(user_id: str) -> Usuario:
+        return await Usuario.get(user_id)
+    
