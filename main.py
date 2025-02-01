347064174aca12afad593bb0888900bc61004d9d
Vivixing <echeverri.vivian1120@gmail.com>
2025-02-01 16:55:27 -0500

    Agregada route de Usuario

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from core.database import init_db
+from routers.UsuarioRouter import router as usuario_router
 
 #Instancia la clase FastAPI
 app = FastAPI()
+
+app.include_router(usuario_router)
 
 #Decorador que indica la ruta de la petición
 @app.get("/")
 async def get_root():
     await init_db()
-    return {"message": "Database initialized"}
+    return {"message": "Base de datos inicializada"}
