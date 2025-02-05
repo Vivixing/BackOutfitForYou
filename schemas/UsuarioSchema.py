d5967321cf103edc81262c88d1cc085218651402
Vivixing <echeverri.vivian1120@gmail.com>
2025-02-04 20:01:24 -0500

    Agregado Validaciones en el schema de Usuario para el campo de nombre

--- schemas/UsuarioSchema.py
+++ schemas/UsuarioSchema.py
@@ -1,10 +1,23 @@
-from pydantic import BaseModel, EmailStr, Field
+from fastapi import HTTPException
+from pydantic import BaseModel, EmailStr, Field, field_validator
+
 
 class UsuarioCreadoRequest(BaseModel):
-    nombre: str = Field(..., pattern="^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$", min_length=2, max_length=30)
+    nombre: str = Field(..., min_length=2, max_length=30)
     email: EmailStr
     contrasena: str
+
+    @field_validator("nombre", mode="before")
+    def nombre_validator(cls, value):
+        if not value.replace(" ", "").isalpha():
+            raise HTTPException(status_code=422, detail="El nombre no puede contener números ni caracteres especiales")
+        if len(value) < 2:
+            raise HTTPException(status_code=422, detail="El nombre debe tener al menos 2 caracteres")
+        if len(value) > 30:
+            raise HTTPException(status_code=422, detail="El nombre no puede tener más de 30 caracteres")
+        return value
 
 class UsuarioLoginRequest(BaseModel):
     email: EmailStr
     contrasena: str
+
