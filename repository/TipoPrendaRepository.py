6f2ed8f2693ce2a8dcfa7acc2aad047d75f87dc7
Vivixing <echeverri.vivian1120@gmail.com>
2025-02-06 07:48:42 -0500

    Creado repository de tipoPrenda

---
+++ repository/TipoPrendaRepository.py
@@ -0,0 +1,20 @@
+from beanie import PydanticObjectId
+from models.TipoPrendaModel import TipoPrenda
+
+class TipoPrensaRepository:
+
+    @staticmethod
+    async def create_tipo_prenda(new_tipo_prenda: TipoPrenda) -> TipoPrenda:
+        return await new_tipo_prenda.insert()
+    
+    @staticmethod
+    async def find_tipo_prenda_by_id(id: PydanticObjectId) -> TipoPrenda:
+        return await TipoPrenda.find_one(TipoPrenda.id == id)
+    
+    @staticmethod
+    async def find_tipo_prenda_by_category(categoria: str) -> TipoPrenda:
+        return await TipoPrenda.find_one(TipoPrenda.categoria == categoria)
+    
+    @staticmethod
+    async def find_all_tipo_prendas() -> list[TipoPrenda]:
+        return await TipoPrenda.all().to_list()
