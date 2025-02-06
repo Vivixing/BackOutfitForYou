ef8bf0dcaa32456f73cff4451ffd85b524206553
Vivixing <echeverri.vivian1120@gmail.com>
2025-02-06 18:13:53 -0500

    completado controller de tipoPrenda

--- controllers/TipoPrendaController.py
+++ controllers/TipoPrendaController.py
@@ -8,7 +8,6 @@ class TipoPrendaController:
 
     async def create_tipo_prenda(request:TipoPrendaCreadoRequest):
         try:
-            #Convertir el request de un BaseModel a un objeto de tipo TipoPrenda (Document)
             tipo_prenda_convert = TipoPrenda(**request.dict())  
             tipo_prenda = await TipoPrendaService.create_tipo_prenda(tipo_prenda_convert)
             return {"status": 200, "message": "Tipo de prenda creado correctamente", "tipo_prenda_id":str(tipo_prenda.id), "data": tipo_prenda}
@@ -19,5 +18,20 @@ class TipoPrendaController:
         try:
             tipo_prenda = await TipoPrendaService.find_tipo_prenda_by_id(id)
             return {"status": 200, "message": "Tipo de prenda encontrado", "data": tipo_prenda}
+        except Exception as e:
+            raise HTTPException(status_code=404, detail=str(e))
+    
+    async def get_all_tipo_prendas():
+        try:
+            tipo_prendas = await TipoPrendaService.find_all_tipo_prendas()
+            return {"status": 200, "message": "Tipos de prendas encontrados", "data": tipo_prendas}
+        except Exception as e:
+            raise HTTPException(status_code=404, detail=str(e))
+    
+    async def update_tipo_prenda(request:TipoPrendaActualizadoRequest):
+        try:
+            tipo_prenda_convert = TipoPrenda(**request.dict())
+            tipo_prenda = await TipoPrendaService.uptade_tipo_prenda(tipo_prenda_convert)
+            return {"status": 200, "message": "Tipo de prenda actualizado correctamente", "tipo_prenda_id":str(tipo_prenda.id), "data": tipo_prenda}
         except Exception as e:
             raise HTTPException(status_code=404, detail=str(e))
