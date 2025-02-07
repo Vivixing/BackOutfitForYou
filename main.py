34156054e605ab420db4d434dfb3b2c9c7046354
Vivixing <echeverri.vivian1120@gmail.com>
2025-02-07 15:22:10 -0500

    Agregado los routers de tipoPrenda y Prenda al main

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from core.database import init_db
 from routers.UsuarioRouter import router as usuario_router
+from routers.TipoPrendaRouter import routerTipoPrenda as tipo_prenda_router
+from routers.PrendaRouter import routerPrenda as prenda_router
 
 #Instancia la clase FastAPI
 app = FastAPI()
 
 app.include_router(usuario_router)
+app.include_router(tipo_prenda_router)
+app.include_router(prenda_router)
 
 #Decorador que indica la ruta de la petición
 @app.get("/")
