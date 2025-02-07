86df3d368a9d8b1f778a1b07b8030416a725cbb3
Vivixing <echeverri.vivian1120@gmail.com>
2025-02-07 15:31:32 -0500

    Modificado TipoPrendaSchema

--- schemas/TipoPrendaSchema.py
+++ schemas/TipoPrendaSchema.py
@@ -1,11 +1,11 @@
-import datetime
+import datetime 
 from enums.CategoriaEnums import CategoriaEnums
 from pydantic import BaseModel, Field
 
 class TipoPrendaCreadoRequest(BaseModel):
     categoria: CategoriaEnums = Field(...)
-    fecha_creacion = Field(default_factory=datetime.datetime.now)
+    fecha_creacion: datetime.datetime = Field(default_factory=datetime.datetime.now)
 
 class TipoPrendaActualizadoRequest(BaseModel):
     categoria: CategoriaEnums = Field(...)
-    fecha_modificacion = Field(default_factory=datetime.datetime.now)
+    fecha_modificacion : datetime.datetime = Field(default_factory=datetime.datetime.now)
