95da7bd27d64bcfab30b033a66a9bba0709aeea4
Vivixing <echeverri.vivian1120@gmail.com>
2025-02-09 21:34:57 -0500

    Correccción de lo tipoPrenda y del enum de Categoría

--- schemas/TipoPrendaSchema.py
+++ schemas/TipoPrendaSchema.py
@@ -1,11 +1,28 @@
-import datetime 
+import datetime
+
+from beanie import PydanticObjectId
+from fastapi import HTTPException 
 from enums.CategoriaEnums import CategoriaEnums
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 class TipoPrendaCreadoRequest(BaseModel):
-    categoria: CategoriaEnums = Field(...)
+    categoria: str = Field(...)
     fecha_creacion: datetime.datetime = Field(default_factory=datetime.datetime.now)
 
+    @field_validator("categoria")
+    def validate_categoria(cls, value):
+        categorias_validas = {e.value for e in CategoriaEnums}
+        if value not in categorias_validas:
+            raise HTTPException(status_code=422, detail=f"Categoría inválida: '{value}'. Debe ser una de {categorias_validas}")
+        return value
+
 class TipoPrendaActualizadoRequest(BaseModel):
-    categoria: CategoriaEnums = Field(...)
+    categoria: str = Field(...)
     fecha_modificacion : datetime.datetime = Field(default_factory=datetime.datetime.now)
+
+    @field_validator("categoria")
+    def validate_categoria(cls, value):
+        categorias_validas = {e.value for e in CategoriaEnums}
+        if value not in categorias_validas:
+            raise HTTPException(status_code=422, detail=f"Categoría inválida: '{value}'. Debe ser una de {categorias_validas}")
+        return value
