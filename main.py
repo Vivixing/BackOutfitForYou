04ff3363b84c1368284e0af6b1ad29b641f1eb23
Vivixing <echeverri.vivian1120@gmail.com>
2025-07-17 19:29:12 -0500

    Cambios en la sección de user y agregado cors

--- main.py
+++ main.py
@@ -4,25 +4,34 @@ from core.modelLoader import load_h5_model_main
 from routers.UsuarioRouter import router as usuario_router
 from routers.TipoPrendaRouter import routerTipoPrenda as tipo_prenda_router
 from routers.PrendaRouter import routerPrenda as prenda_router
+from fastapi.middleware.cors import CORSMiddleware
 
 #Instancia la clase FastAPI
 app = FastAPI()
 # Inicializar modelo una sola vez
 model = None
 
+# Lista de orígenes permitidos
+origins = [
+    "http://localhost:8100",  # frontend Ionic
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],            # <-- or ["*"] para permitir todos
+    allow_credentials=True,
+    allow_methods=["*"],              # <-- importante para permitir OPTIONS
+    allow_headers=["*"],
+)
+
 app.include_router(usuario_router)
 app.include_router(tipo_prenda_router)
 app.include_router(prenda_router)
 
-#Decorador que indica la ruta de la petición
-@app.get("/")
-async def get_root():
-    await init_db()
-    return {"message": "Base de datos inicializada"}
-
 @app.on_event("startup")
 async def startup_event():
     global model
+    await init_db() 
     model = await load_h5_model_main()
-    return {"message": "Inicializado Modelo"}
+    return {"message": "Inicialización completa"}
 
--- services/UsuarioService.py
+++ services/UsuarioService.py
@@ -2,6 +2,7 @@ from models.UsuarioModel import Usuario
 from repository.UsuarioRepository import UsuarioRepository
 from beanie import PydanticObjectId
 from pydantic import EmailStr
+from fastapi import HTTPException
 
 class UsuarioService:
     
@@ -9,31 +10,31 @@ class UsuarioService:
     async def create_user(new_user: Usuario) -> Usuario:
         exist_name = await UsuarioRepository.find_user_by_name(new_user.nombre)   
         if exist_name:
-            raise Exception("Ya existe un usuario con ese nombre")
+            raise HTTPException(status_code="400", detail="Ya existe un usuario con ese nombre")
         exist_email = await UsuarioRepository.find_user_by_email(new_user.email)
         if exist_email:
-            raise Exception("El usuario ya está registrado")
+            raise HTTPException(status_code="400", detail="El usuario ya está registrado")
         return await UsuarioRepository.create_user(new_user)
     
     @staticmethod
     async def find_user_by_email(email: EmailStr) -> Usuario:
         exist_email_user = await UsuarioRepository.find_user_by_email(email)
         if not exist_email_user:
-            raise Exception("No exite un usuario con ese email")
+            raise HTTPException(status_code="400", detail="No existe un usuario registrado con ese email")
         return exist_email_user
     
     @staticmethod
     async def find_user_by_id(user_id: PydanticObjectId) -> Usuario:
         exist_user_id = await UsuarioRepository.find_user_by_id(user_id)
         if not exist_user_id:
-            raise Exception("No exite un usuario con ese ID")
+            raise HTTPException(status_code="400", detail="No existe un usuario con ese ID")
         return exist_user_id
     
     @staticmethod
     async def login_user(email: EmailStr, password: str) -> Usuario:
         exist_user_login = await UsuarioRepository.find_user_by_email(email)
         if not exist_user_login:
-            raise Exception("No exite un usuario con ese email")
+            raise HTTPException(status_code="400", detail="Email o contraseña inválidos")
         if exist_user_login.contrasena != password:
-            raise Exception("Contraseña incorrecta")
+            raise HTTPException(status_code="400", detail="Email o contraseña inválidos")
         return exist_user_login
