ac69173f25fe22d9c94444270879825d01fcf5b5
Vivixing <echeverri.vivian1120@gmail.com>
2025-09-13 10:54:20 -0500

    Agregado función para traer vestuario por id y por usuario

--- repository/VestuarioRepository.py
+++ repository/VestuarioRepository.py
@@ -6,3 +6,11 @@ class VestuarioRepository:
     async def create_vestuario(vestuario: Vestuario) -> Vestuario:
         return await Vestuario.insert(vestuario)
     
+    @staticmethod
+    async def get_vestuario_by_id(vestuario_id: str) -> Vestuario | None:
+        return await Vestuario.get(vestuario_id)
+    
+    @staticmethod
+    async def get_vestuario_by_usuario(usuario_id: str) -> list[Vestuario]:
+        return await Vestuario.find(Vestuario.usuarioId == usuario_id).to_list()
+    
