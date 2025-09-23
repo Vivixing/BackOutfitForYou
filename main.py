2fc7bb8e8a87fe155eac018f82f429a636541cc8
Vivixing <echeverri.vivian1120@gmail.com>
2025-09-23 13:50:20 -0500

    Actualizado

--- main.py
+++ main.py
@@ -46,5 +46,5 @@ async def startup_event():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
